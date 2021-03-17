70fa7e9987ddda16db5ec707664790e510424c74
mvaradi <mvaradi@ebi.ac.uk>
2021-03-17 15:29:25 +0000

    Initial commit

---
+++ src/app/csv-exporter/csv-exporter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-csv-exporter',
+  templateUrl: './csv-exporter.component.html',
+  styleUrls: ['./csv-exporter.component.css']
+})
+export class CsvExporterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
