8ae7441088bd000c41dcdea023513f6d19121e34
mvaradi <mvaradi@ebi.ac.uk>
2021-03-17 15:54:03 +0000

    Fixing test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import {TestBed, async} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {CsvExporterComponent} from './csv-exporter/csv-exporter.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        CsvExporterComponent
       ],
     }).compileComponents();
   }));
